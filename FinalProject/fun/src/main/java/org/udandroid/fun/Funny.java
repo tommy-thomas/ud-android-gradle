c6624132116552d88b45bdb2764e5e3860de30a1
tommy-thomas <tommyt@uchicago.edu>
2018-07-20 12:50:26 -0500

    first commit

---
+++ FinalProject/fun/src/main/java/org/udandroid/fun/Funny.java
@@ -0,0 +1,5 @@
+package org.udandroid.fun;
+
+public class Funny {
+
+}
---
+++ FinalProject/fun/src/main/java/org/udandroid/fun/Joke.java
@@ -0,0 +1,20 @@
+package org.udandroid.fun;
+
+/**
+ * Created by tommy-thomas on 7/16/18.
+ */
+
+public class Joke {
+
+    private String Joke;
+    private String PunchLine;
+
+    public String getJoke(){
+        return Joke;
+    }
+
+    public String getPunchLine(){
+        return PunchLine;
+    }
+
+}
